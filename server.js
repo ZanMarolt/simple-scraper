638c96bb258dbda000952209daa22ffdde5e0adf
Zan Marolt <marolt.zan@hotmail.com>
2016-07-08 14:46:44 +0200

    Getting html out of an url

--- server.js
+++ server.js
@@ -5,28 +5,22 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-    // The URL we will scrape from - in our example Anchorman 2.
-
+    
     url = 'http://www.imdb.com/title/tt1229340/';
 
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
-
     request(url, function(error, response, html){
-
-        // First we'll check to make sure no errors occurred when making the request
-        console.log('request!')
-
         if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
             var $ = cheerio.load(html);
-
-            // Finally, we'll define the variables we're going to capture
 
             var title, release, rating;
             var json = { title : "", release : "", rating : ""};
+
+            var test = $('.title-wrapper').children().text();
+            console.log(test)
+
+            res.send('Check console')
+
+            
         }
     })
 })
